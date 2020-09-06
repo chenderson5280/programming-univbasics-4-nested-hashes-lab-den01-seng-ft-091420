458bf46d3366dd48b1467c58398ca7c768c10c06
Christopher Henderson <chenderson5280@gmail.com>
2020-09-06 02:47:00 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -124,6 +124,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-			programmer_hash[:dennis_ritchie][:languages]
+			programmer_hash[:dennis_ritchie][:languages]+="Array"
 			programmer_hash
 end
